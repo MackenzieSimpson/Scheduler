3e90f2764383dea088c4743e7ada815ad1f51585
MackenzieSimpson <mcsimpson@my.milligan.edu>
2019-04-30 17:58:44 -0400

    link lists working

--- Test and Project Database.cpp
+++ Test and Project Database.cpp
@@ -3,7 +3,7 @@
 // Email Address:	mcsimpson@my.milligan.edu
 // Assignment:		Term Project
 // Description:		Program to manage assignment dates
-// Last Changed:	April 12, 2019
+// Last Changed:	April 30, 2019
 
 #include "assignmentDate.h"
 #include "stdafx.h"
@@ -20,10 +20,12 @@
 
 
 
+
 assignmentDate date;
 assignmentType assignment;
 assignmentProf prof(0,221);
 assignmentTest test;
+
 
 
 void listPrint(string ProfessorName[], int SizeArray);
@@ -57,7 +59,8 @@ int main()
 		cout << "Choose 1 to input date and assignment. \n";
 		cout << "Choose 2 to look at assignment log.\n";
 		cout << "Choose 3 to look at Professor List.\n";
-		cout << "Choose 4 to exit the program.\n";
+		cout << "Choose 4 to look at Link List.\n";
+		cout << "Choose 5 to exit the program.\n";
 		cout << "Enter your choice and press return.\n";
 		cin >> choice;
 
@@ -114,9 +117,160 @@ int main()
 			case 3:
 			{
 				listPrint(ProfessorName, SizeArray);
+				
+				
+			
+			
+
 				break;
 			}
 			case 4:
+			{struct node
+			{
+				int data;
+				node *next;
+			};
+			class list
+			{
+			private:
+				node *head, *tail;
+			public:
+				list()
+				{
+					head = NULL;
+					tail = NULL;
+				}
+				void createnode(int value)
+				{
+					node *temp = new node;
+					temp->data = value;
+					temp->next = NULL;
+					if (head == NULL)
+					{
+						head = temp;
+						tail = temp;
+						temp = NULL;
+					}
+					else
+					{
+						tail->next = temp;
+						tail = temp;
+					}
+				}
+				void display()
+				{
+					node *temp = new node;
+					temp = head;
+					while (temp != NULL)
+					{
+						cout << temp->data << "\t";
+						temp = temp->next;
+					}
+				}
+				void insert_start(int value)
+				{
+					node *temp = new node;
+					temp->data = value;
+					temp->next = head;
+					head = temp;
+				}
+				void insert_position(int pos, int value)
+				{
+					node *pre = new node;
+					node *cur = new node;
+					node *temp = new node;
+					cur = head;
+					for (int i = 1; i < pos; i++)
+					{
+						pre = cur;
+						cur = cur->next;
+					}
+					temp->data = value;
+					pre->next = temp;
+					temp->next = cur;
+				}
+				void delete_first()
+				{
+					node *temp = new node;
+					temp = head;
+					head = head->next;
+					delete temp;
+				}
+				void delete_last()
+				{
+					node *current = new node;
+					node *previous = new node;
+					current = head;
+					while (current->next != NULL)
+					{
+						previous = current;
+						current = current->next;
+					}
+					tail = previous;
+					previous->next = NULL;
+					delete current;
+				}
+				void delete_position(int pos)
+				{
+					node *current = new node;
+					node *previous = new node;
+					current = head;
+					for (int i = 1; i < pos; i++)
+					{
+						previous = current;
+						current = current->next;
+					}
+					previous->next = current->next;
+				}
+			};
+			
+			{
+				list obj;
+				obj.createnode(25);
+				obj.createnode(50);
+				obj.createnode(90);
+				obj.createnode(40);
+				cout << "\n--------------------------------------------------\n";
+				cout << "---------------Displaying All nodes---------------";
+				cout << "\n--------------------------------------------------\n";
+				obj.display();
+				cout << "\n--------------------------------------------------\n";
+				cout << "-----------------Inserting At End-----------------";
+				cout << "\n--------------------------------------------------\n";
+				obj.createnode(55);
+				obj.display();
+				cout << "\n--------------------------------------------------\n";
+				cout << "----------------Inserting At Start----------------";
+				cout << "\n--------------------------------------------------\n";
+				obj.insert_start(50);
+				obj.display();
+				cout << "\n--------------------------------------------------\n";
+				cout << "-------------Inserting At Particular--------------";
+				cout << "\n--------------------------------------------------\n";
+				obj.insert_position(5, 60);
+				obj.display();
+				cout << "\n--------------------------------------------------\n";
+				cout << "----------------Deleting At Start-----------------";
+				cout << "\n--------------------------------------------------\n";
+				obj.delete_first();
+				obj.display();
+				cout << "\n--------------------------------------------------\n";
+				cout << "-----------------Deleing At End-------------------";
+				cout << "\n--------------------------------------------------\n";
+				obj.delete_last();
+				obj.display();
+				cout << "\n--------------------------------------------------\n";
+				cout << "--------------Deleting At Particular--------------";
+				cout << "\n--------------------------------------------------\n";
+				obj.delete_position(4);
+				obj.display();
+				cout << "\n--------------------------------------------------\n";
+				system("pause");
+				return 0;
+			}
+				break;
+			}
+			case 5:
 			{ 
 			exit (0);
 			break;
@@ -127,7 +281,7 @@ int main()
 		}
 	}
 		while (choice >= 1 && choice <= 4);
-		if (choice != 1 || choice != 2 || choice != 3|| choice!=4)
+		if (choice != 1 || choice != 2 || choice != 3|| choice!=4 || choice!=5)
 
 		{
 			cout << "The choice entered is not an option on the menu, please exit the program and enter 1,2, or 3.\n";
