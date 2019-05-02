be68f7135583c851068783190a0135e42df39c27
MackenzieSimpson <mcsimpson@my.milligan.edu>
2019-05-02 10:45:22 -0400

    working

--- assignmentDate.cpp
+++ assignmentDate.cpp
@@ -132,6 +132,24 @@ void assignmentDate::inputDates()
 	mfile.open("dates2data.txt", std::ofstream::app);
 	mfile << monthVal << "," << dayVal << "," << yearVal << endl;
 	mfile.close();
+	{
+
+		int difference;
+		int month, month2, day, day2, year, year2, jdate, jdate2, diff;
+		const int daysInYear[] = { 0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365 };
+		const int daysInLeapYear[] = { 0, 0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366 };
+		const int daysInMonth[] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		const int daysInMonthLeap[] = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+		// You could do your calculations berfore you store the values and have just one.
+		jdate = (yearVal - 1900) * 365 + daysInYear[monthVal] + dayVal;
+		jdate2 = (second_date_year - 1900) * 365 + daysInYear[second_date_month] + second_date_days;
+
+		difference = abs(jdate - jdate2);
+		cout << "The number of days between these dates is: " << difference << endl;
+
+		system("pause");
+	}
 }
 void assignmentDate::outputDates()
 {
